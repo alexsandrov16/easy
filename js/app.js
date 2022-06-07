dc49ecf903e80d89b38db5df7f8459a7e75e2157
alexsandrov16 <alejandro920131@gmail.com>
2022-06-06 22:56:40 -0400

    Second Commit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,54 @@
-!function () {
-    let modal = document.querySelector('dialog');
-    if (!window.localStorage.getItem('user')) {
-        modal.setAttribute('open', '');
-    } else {
-        window.location.href = "http://localhost/easy/quiz.html";
+! function () {
+    console.log('Ready...');
+    if (window.location != 'http://localhost/easy/' && !window.localStorage.getItem('user')) {
+        window.location.href = 'http://localhost/easy/';
     }
 }();
 
-function saveName() {
-    let input = document.querySelector('input');
-    let cancel = document.querySelector('a#cancel');
-    let confirm = document.querySelector('a#confirm');
+class Modal {
+    modal;
+    input;
+    cancel;
+    confirm;
+    constructor(config) {
+        this.modal = document.querySelector('dialog');
+        this.input = document.querySelector('input');
+        this.cancel = document.querySelector('a#cancel');
+        this.confirm = document.querySelector('a#confirm');
 
-    cancel.addEventListener('click', () => {
-        input.value = '';
-    });
+        this.url = config.url;
 
-    confirm.addEventListener('click', () => {
-        if (input.value == '') {
-            input.setAttribute('aria-invalid', true);
-        } else {
-            window.localStorage.setItem('user', input.value);
-            window.location.href = "http://localhost/easy/quiz.html";
-        }
-    });
+        this.close();
+        this.send();
+    }
+
+    open(el) {
+        el.addEventListener('click',()=>{
+            if (!window.localStorage.getItem('user')) {
+                this.modal.setAttribute('open', '');
+                this.input.focus();
+            } else {
+                window.location.href = this.url;
+            }
+        });
+    }
+
+    close() {
+        this.cancel.addEventListener('click', () => {
+            this.modal.removeAttribute('open');
+            this.input.value = '';
+        });
+    }
+
+    send(){
+        this.confirm.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (this.input.value == '') {
+                this.input.setAttribute('aria-invalid', true);
+            } else{
+                window.localStorage.setItem('user', this.input.value);
+                window.location.href = this.url;
+            }
+        });
+    }
 }
